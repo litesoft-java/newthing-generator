9ac8e9de92a76f6d06688757946a7b47009cc83f
george.smith <litesoft@gmail.com>
2022-09-01 09:48:20 -0700

    Force Tag push

--- sampleNewThingsTemplate/test.../NewThingImplTest.java
+++ sampleNewThingsTemplate/test.../NewThingImplTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class NewThingImplTest {
+
     @Test
     void testGetters() {
         NewThing org = new NewThingImpl();
